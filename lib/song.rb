8348b8bc2fb0580de06029087adc9162ca03c52c
Amanda Ackerman <ackerm44@gmail.com>
2017-11-29 14:38:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,7 +60,7 @@ class Song
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     #sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, name)
   end
 
 end
